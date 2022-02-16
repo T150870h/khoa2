5048e5b393f8790780cd890a3da90d647edef231
T150870h <90712503+T150870h@users.noreply.github.com>
2022-02-16 22:24:24 +0700

    khoa2

--- chatapp/componnenst/inputGroup.js
+++ chatapp/componnenst/inputGroup.js
@@ -1,33 +1,42 @@
 class InputGroup {
-    $inputGroup;
-    $input;
+    // $inputGroup;
+    // $input;
     constructor(lableName, inputType, Placeholder) {
-        
+
         this.$inputGroup = document.createElement('form');
-        this.$inputGroup.setAttribute('class', 'flex flex-col mb-4' ) 
+        this.$inputGroup.setAttribute('class', 'flex flex-col mb-4')
 
         this.$label = document.createElement('label');
         this.$label.innerText = lableName;
-        this.$label.setAttribute('class', 'text-white text-lg mb-2 font-bold' );
-        
+        this.$label.setAttribute('class', 'text-white text-lg mb-2 font-bold');
+
         this.$input = document.createElement('input');
-        this.$input.setAttribute('class','py-2 px4 rounded-lg');
+        this.$input.setAttribute('class', 'py-2 px4 rounded-lg');
         this.$input.type = inputType;
         this.$input.placeholder = Placeholder;
 
-       
+        this.$errorMsg = document.createElement('span');
+        this.$errorMsg.setAttribute('class', 'text-black text-sm');
+
+
     }
     getValue() {
-        return this.$input.value; 
+        return this.$input.value;
     }
-   
+  
+  setErrorMsg(msg) {
+        this.$errorMsg.innerText = msg;
+    }
+
+
     render() {
-       this.$inputGroup.appendChild(this.$label);
-       this.$inputGroup.appendChild(this.$input);
-         return this.$inputGroup;
+        this.$inputGroup.appendChild(this.$label);
+        this.$inputGroup.appendChild(this.$input);
+        this.$inputGroup.appendChild(this.$errorMsg);
+        return this.$inputGroup;
     }
-      
-    }
+
+}
 
 
 
