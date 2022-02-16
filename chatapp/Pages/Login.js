1f37229be1cbd71cc11dae2970e785c5f67f4a80
T150870h <90712503+T150870h@users.noreply.github.com>
2022-02-16 22:27:15 +0700

    khoa2

--- chatapp/Pages/Login.js
+++ chatapp/Pages/Login.js
@@ -1,19 +1,19 @@
-import {signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js'
+import { signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js'
 import Register from './Register.js';
 import InputGroup from '../componnenst/inputGroup.js';
 import app from '../index.js';
-import {auth} from '../constants/commons.js';
+import { auth } from '../constants/commons.js';
 import Main from '../Pages/Main.js';
 
 
 
 
 class Login {
-  $emailGroup;
-  $passwordGroup;
-   constructor() {
-       this.$formContainer = document.createElement('form');
-      this.$formContainer.setAttribute('class','w-1/3 mx-auto p-8 mt-12 bg-blue-400 rounded-lg w-96' );
+  // $emailGroup;
+  // $passwordGroup;
+  constructor() {
+    this.$formContainer = document.createElement('form');
+    this.$formContainer.setAttribute('class', 'w-1/3 mx-auto p-8 mt-12 bg-blue-400 rounded-lg w-96');
     //    this.$emailLabel = document.createElement('label');
     //      this.$emailLabel.innerText = 'Email';
     //      this.$emailInput = document.createElement('input');
@@ -24,35 +24,41 @@ class Login {
     //      this.$passwordInput = document.createElement('input');
     //      this.$passwordInput.placeholder = 'Password';
     this.$formContainer.addEventListener('submit', this.onSubmit);
-         
-    this.$emailGroup = new InputGroup('Email', 'email', 'Email');
-    this.$passwordGroup = new InputGroup('Password', 'password', 'Password');
-         
-         this.$loginButton = document.createElement('button');
-            this.$loginButton.innerText = 'Login';
-            this.$loginButton.setAttribute('class','py-2 px-8 rounded-lg bg-yellow-400 text-black hover:bg-yellow-500');
-       
-            this.$goToRegister = document.createElement('span');
-            this.$goToRegister.innerText = 'Not a member?';
-            this.$goToRegister.setAttribute('class', 'text-white ml-4 cursor-pointer ');
-            this.$goToRegister.addEventListener('click', this.goToRegisterPage);
+
+    this.$email = new InputGroup('Email', 'email', 'Email');
+    this.$password = new InputGroup('Password', 'password', 'Password');
+
+    this.$loginButton = document.createElement('button');
+    this.$loginButton.type = 'submit';
+    this.$loginButton.innerText = 'Login';
+    this.$loginButton.setAttribute('class', 'py-2 px-8 rounded-lg bg-yellow-400 text-black hover:bg-yellow-500');
+
+    this.$goToRegister = document.createElement('span');
+    this.$goToRegister.innerText = 'Not a member?';
+    this.$goToRegister.setAttribute('class', 'text-white ml-4 cursor-pointer ');
+    this.$goToRegister.addEventListener('click', this.goToRegisterPage);
 
 
 
-   }
-   goToRegisterPage = () => {
-       const registerScreen = new Register();
-       app.setActiveScreen(registerScreen);
+  }
+  goToRegisterPage = () => {
+    const registerScreen = new Register();
+    app.setActiveScreen(registerScreen);
 
-   }
-    onSubmit = async (event) => {
-        event.preventDefault();
-     try {
-      const email = this.$emailGroup.getValue();
-      const password = this.$passwordGroup.getValue();
+  }
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const email = this.$email.getValue();
+      console.log('email', this.$email.setErrorMsg);
+      const password = this.$password.getValue();
+      //  if( email.length === 0) {
+      //      this.$email.setErrorMsg('Email is required');
+      //  }
 
       if (email && password) {
-        const response =  await signInWithEmailAndPassword(auth, email, password);
+        const response = await signInWithEmailAndPassword(auth, email, password);
         const user = response.user;
         if (user) {
           //  const mainScreen = new Main();
@@ -60,22 +66,22 @@ class Login {
           alert('Login Successful');
         }
       }
-     }catch(error) {
-       alert(error.message);
-     }
-    };
-   render(container) {
-        //  this.$formContainer.appendChild(this.$emailLabel);
-        //  this.$formContainer.appendChild(this.$emailInput);
-        //  this.$formContainer.appendChild(this.$passwordLabel);
-        //  this.$formContainer.appendChild(this.$passwordInput);
-        this.$formContainer.appendChild(this.$emailGroup.render());
-        this.$formContainer.appendChild(this.$passwordGroup.render());
-         this.$formContainer.appendChild(this.$loginButton);
-         this.$formContainer.appendChild(this.$goToRegister);
-           container.appendChild(this.$formContainer); // 
+    } catch (error) {
+      alert(error.message);
     }
-   
-           
-   }
-     export default  Login;
+  };
+  render(container) {
+    //  this.$formContainer.appendChild(this.$emailLabel);
+    //  this.$formContainer.appendChild(this.$emailInput);
+    //  this.$formContainer.appendChild(this.$passwordLabel);
+    //  this.$formContainer.appendChild(this.$passwordInput);
+    this.$formContainer.appendChild(this.$email.render());
+    this.$formContainer.appendChild(this.$password.render());
+    this.$formContainer.appendChild(this.$loginButton);
+    this.$formContainer.appendChild(this.$goToRegister);
+    container.appendChild(this.$formContainer); // 
+  }
+
+
+}
+export default Login;
